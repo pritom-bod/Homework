5c52e19833dd65d245a3e861d90428a8a78d7eb3
pritom-bod <pritomsarker406@gmail.com>
2025-04-28 15:04:31 +0600

    full commit

---
+++ ClassTestOne/ClassTestOne/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ClassTestOne project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClassTestOne.settings')
+
+application = get_wsgi_application()
