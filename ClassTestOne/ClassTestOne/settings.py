5351a852e0f9d831fc61dafa1a759d2274bdd98f
pritom-bod <pritomsarker406@gmail.com>
2025-05-09 01:14:13 +0600

    add migration

--- ClassTestOne/ClassTestOne/settings.py
+++ ClassTestOne/ClassTestOne/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = BASE_DIR/'templates'
-
+CSRF_TRUSTED_ORIGINS= [""]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
